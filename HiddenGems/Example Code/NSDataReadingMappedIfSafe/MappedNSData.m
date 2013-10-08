80652d9c5067f0f41b3737d1e39868b5c45e3a14
Andrew Madsen <andrew@openreelsoftware.com>
2013-10-07 20:24:33 -0600

    Added small example program for mapped NSData gem.

---
+++ HiddenGems/Example Code/NSDataReadingMappedIfSafe/MappedNSData.m
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[]) {
+	@autoreleasepool {
+
+		NSString *filePath = [@"~/Desktop/bigfile" stringByExpandingTildeInPath];
+		//NSData *data = [[NSData alloc] initWithContentsOfFile:filePath options:0 error:NULL]; // Normal, non-mapped
+		NSData *data = [[NSData alloc] initWithContentsOfFile:filePath options:NSDataReadingMappedIfSafe error:NULL]; // Mapped
+		
+		NSData *last10Bytes = [data subdataWithRange:NSMakeRange([data length]-10, 10)];
+		NSInteger sum = 0;
+		int8_t *bytes = (int8_t *)[last10Bytes bytes];
+		for (NSUInteger i=0; i<[last10Bytes length]; i++) { sum += *(bytes+i); }
+
+		NSLog(@"Number of bytes: %lu sum: %li", (unsigned long)numBytes, (long)sum);
+		
+		while(1) {}
+		[data release];
+	}
+}
