a129cbce2731431a750c761d368f39bb027529cc
Andrew Madsen <andrew@openreelsoftware.com>
2013-10-07 20:45:48 -0600

    Added comment explaining how to generate 'bigfile' to MappedNSData.

--- HiddenGems/Example Code/NSDataReadingMappedIfSafe/MappedNSData.m
+++ HiddenGems/Example Code/NSDataReadingMappedIfSafe/MappedNSData.m
@@ -3,22 +3,25 @@
 int main(int argc, char *argv[]) {
 	@autoreleasepool {
 
-NSString *filePath = [@"~/Desktop/bigfile" stringByExpandingTildeInPath]; // ~1GB
-//NSData *data = [[NSData alloc] initWithContentsOfFile:filePath 
-options:0 
-error:NULL]; // Normal, non-mapped
-NSData *data = [[NSData alloc] initWithContentsOfFile:filePath 
-											   options:NSDataReadingMappedIfSafe 
-											     error:NULL]; // Mapped
+		// You can generate some random, big file with a terminal command like:
+		// dd if=/dev/urandom of=~/Desktop/bigfile bs=1000000 count=1000
 
-NSData *last10Bytes = [data subdataWithRange:NSMakeRange([data length]-10, 10)];
-NSInteger sum = 0;
-int8_t *bytes = (int8_t *)[last10Bytes bytes];
-for (NSUInteger i=0; i<[last10Bytes length]; i++) { sum += *(bytes+i); }
+		NSString *filePath = [@"~/Desktop/bigfile" stringByExpandingTildeInPath]; // ~1GB
+		//NSData *data = [[NSData alloc] initWithContentsOfFile:filePath 
+		options:0 
+		error:NULL]; // Normal, non-mapped
+		NSData *data = [[NSData alloc] initWithContentsOfFile:filePath 
+													   options:NSDataReadingMappedIfSafe 
+													     error:NULL]; // Mapped
 
-NSLog(@"Number of bytes: %lu sum: %li", (unsigned long)numBytes, (long)sum);
+		NSData *last10Bytes = [data subdataWithRange:NSMakeRange([data length]-10, 10)];
+		NSInteger sum = 0;
+		int8_t *bytes = (int8_t *)[last10Bytes bytes];
+		for (NSUInteger i=0; i<[last10Bytes length]; i++) { sum += *(bytes+i); }
 
-while(1) {}
-[data release];
+		NSLog(@"Number of bytes: %lu sum: %li", (unsigned long)numBytes, (long)sum);
+
+		while(1) {}
+		[data release];
 	}
 }
